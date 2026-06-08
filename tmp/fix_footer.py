46e5e0eb491adc857821918ff040b9e1bf6eee1d
OptimayRu <optimay.ru@gmail.com>
2026-06-08 08:42:16 +0500

    chore: remove screenshots, tmp, and unused blog post

--- tmp/fix_footer.py
+++
@@ -1,1 +0,0 @@
-import os os.chdir('/home/almaz/StatSites/Astro/fastry') with open('src/components/layout/Footer.astro', 'r') as f: c = f.read() c = c.replace('{item.label}', '{t(item.labelKey, locale)}') with open('src/components/layout/Footer.astro', 'w') as f: f.write(c) print('Done')
